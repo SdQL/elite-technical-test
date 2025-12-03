99335d17b4ff10997cbfa076bf045692c557b2e1
SdQL <sadavid2007@gmail.com>
2025-12-03 11:59:07 -0500

    feat: update main.tsx to implement routing for UsersPage and UserProfilePage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Layout } from './components/Layout'
-import { UserPage } from './components/users/UserPage'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import "./index.css";
+import { Layout } from "./components/Layout";
+import { UsersPage } from "./pages/UsersPage";
+import UserProfilePage from "./pages/UserProfilePage";
 
-createRoot(document.getElementById('root')!).render(
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Layout>
-      <UserPage />
-    </Layout>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Navigate to="/users" replace />} />
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/users/:id" element={<UserProfilePage />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  </StrictMode>
+);
